7e108995d6bb20846a2e75905299f6b9d8b7d503
David Czaplinski <david.czaplinski@ymail.com>
2017-05-28 21:27:28 +0200

    Add deploy script for github page

---
+++ _deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SOURCE_DIR="/home/david/Code/while-true-make-games"
+DEPLOY_DIR="/home/david/Code/while-true-make-games-page"
+
+SAVED_DIR="$(pwd)"
+
+echo "=============================================="
+echo "Build jekyll page"
+echo "=============================================="
+cd $SOURCE_DIR
+jekyll build --baseurl while-true-make-games-page
+
+echo "=============================================="
+echo "Copy page content"
+echo "=============================================="
+cd $DEPLOY_DIR
+rm -Rf ./*
+cp -r $SOURCE_DIR/_site/* .
+
+echo "=============================================="
+echo "Push to github"
+echo "=============================================="
+git add .
+git commit -m "Automatic deploy"
+git push
+
+cd $SAVED_DIR
+
